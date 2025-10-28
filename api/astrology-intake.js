73031ba6312007c19c250421f8656e4785b3fa97
hazcam-nano <51077714+hazcam-nano@users.noreply.github.com>
2025-10-28 19:32:41 +1000

    Update astrology-intake.js

--- api/astrology-intake.js
+++ api/astrology-intake.js
@@ -1,3 +1,4 @@
-  module.exports = (req, res) => {
-    res.status(200).json({ ok: true });
-  }
+export default async function handler(req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).end(JSON.stringify({ ok: true, msg: "health" }));
+}
